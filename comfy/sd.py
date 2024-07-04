0c9bffa463489440896cd4deed7aa3f4f12a6ccd
jasstionzyf <onmygoing@163.com>
2024-07-04 03:23:08 +0000

    updates

--- comfy/sd.py
+++ comfy/sd.py
@@ -2,7 +2,7 @@ import torch
 from enum import Enum
 import logging
 
-from comfy import model_management
+from comfy import model_management  
 from .ldm.models.autoencoder import AutoencoderKL, AutoencodingEngine
 from .ldm.cascade.stage_a import StageA
 from .ldm.cascade.stage_c_coder import StageC_coder
